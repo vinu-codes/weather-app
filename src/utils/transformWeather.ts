a984787e974f634efc894e0a8c71c6641da3f84f
Vinu <133609222+vinu-codes@users.noreply.github.com>
2024-11-30 14:22:49 +1100

    add transformWeather fn

---
+++ src/utils/transformWeather.ts
@@ -0,0 +1,3 @@
+export const transformWeather = () => {
+    // I want to write a function that will create a new structure and store in my state for my weather data, which will include both currentWeather and extendedWeatherData
+}
