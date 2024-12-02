fefa1355ec5dcfdf8bccba95f0d0f66f38953a94
Vinu <133609222+vinu-codes@users.noreply.github.com>
2024-12-02 19:13:17 +1100

    add in icons

--- src/components/CurrentWeather/CurrentWeather.tsx
+++ src/components/CurrentWeather/CurrentWeather.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import WeatherIcon from './WeatherIcon'
 import { useSelector } from 'react-redux'
 import { currentWeather } from '@state/weather'
+import directionImg from '../../assets/weather/direction.png'
+import humid from '../../assets/weather/humid.png'
+import wind from '../../assets/weather/wind.png'
 
 const CurrentWeather = () => {
   const weather = useSelector(currentWeather)
@@ -9,24 +12,47 @@ const CurrentWeather = () => {
   return (
     <div className="bg-white w-full rounded-2xl flex flex-col p-12">
       <div className="text-neutral-500 flex-row">
-        <h2>Current Weather</h2>
+        <h2 className="font-semibold text-lg">Current Weather</h2>
       </div>
       <div className="flex">
         <div className="left-panel flex flex-col flex-1">
-          <span className="text-large">{weather?.city}</span>
+          <span className="text-large font-semibold">{weather?.city}</span>
           <div className="flex items-center">
             <WeatherIcon icon={weather.weather?.icon} />
-            <span className="text-5xl">
+            <span className="text-8xl font-semibold">
               {weather.weather?.temp}
               <sup>&deg;</sup>
             </span>
           </div>
-          <span className="text-large">{weather.weather?.description}</span>
+          <span className="text-large font-semibold text-neutral-500">
+            {weather.weather?.description}
+          </span>
         </div>
         <div className="right-panel flex flex-col flex-1 gap-8">
-          <span>humidity: {weather.weather?.humidity}</span>
-          <span>wind: {weather.weather?.wind?.speed}</span>
-          <span>direction: {weather.weather?.wind?.deg}</span>
+          <span className="text-lg font-semibold flex">
+            <img
+              style={{ width: '24px', height: '24px', marginRight: '8px' }}
+              src={humid}
+              alt="humid"
+            />
+            Humidity {weather.weather?.humidity}
+          </span>
+          <span className="text-lg font-semibold flex">
+            <img
+              style={{ width: '24px', height: '24px', marginRight: '8px' }}
+              src={wind}
+              alt="wind"
+            />
+            Wind {weather.weather?.wind?.speed}
+          </span>
+          <span className="text-lg font-semibold flex">
+            <img
+              style={{ width: '24px', height: '24px', marginRight: '8px' }}
+              src={directionImg}
+              alt="direction"
+            />
+            Wind direction {weather.weather?.wind?.deg}
+          </span>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,6 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    fontSize: {
-      sm: '14px',
-      base: '16px',
-      large: '18px',
-      xl: '20px',
-      '2xl': '24px',
-      '3xl': '28px',
-      '4xl': '32px',
-      '5xl': '36px',
-    },
     extend: {},
   },
   plugins: [],
